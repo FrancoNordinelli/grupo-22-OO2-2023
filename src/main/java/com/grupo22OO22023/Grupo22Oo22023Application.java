1914e12059139e600af5d1e14e1916c78410bb67
FrancoNordinelli <imnordi@gmail.com>
2023-05-29 13:15:02 -0300

    archivo inicial de Spring

---
+++ src/main/java/com/grupo22OO22023/Grupo22Oo22023Application.java
@@ -0,0 +1,13 @@
+package com.grupo22OO22023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo22Oo22023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo22Oo22023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo22OO22023/Grupo22Oo22023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo22OO22023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo22Oo22023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
