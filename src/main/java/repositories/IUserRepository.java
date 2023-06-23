0c4210ffd12a44c560301609d883738bf5938704
FrancoNordinelli <imnordi@gmail.com>
2023-06-23 17:44:18 -0300

    repository

--- src/main/java/repositories/IUserRepository.java
+++ src/main/java/repositories/IUserRepository.java
@@ -1,9 +1,13 @@
 package repositories;
 
+import java.io.Serializable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import entities.User;
+
 @Repository("userRepository")
-public class IUserRepository    {
+public interface IUserRepository extends JpaRepository<User, Serializable>    {
 
 }
