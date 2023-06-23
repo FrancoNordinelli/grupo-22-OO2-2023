0187f3beb5300717b81738646f8fa67f9b02e93b
FrancoNordinelli <imnordi@gmail.com>
2023-06-23 14:50:24 -0300

    Repository para User/Role

--- src/main/java/com/grupo22OO22023/Grupo22Oo22023Application.java
+++ src/main/java/com/grupo22OO22023/Grupo22Oo22023Application.java
@@ -2,8 +2,14 @@ package com.grupo22OO22023;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@Configuration
+@EnableJpaRepositories("paquete.de.repositorios")
+@EntityScan("paquete.de.entidades")
 public class Grupo22Oo22023Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/repositories/IUserRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public class IUserRepository    {
+
+}
