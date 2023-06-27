136d0f4c398d2482cfbbdf93c7660f7d34e941ba
RamiroMagallanG <rmagallangonzalez@gmail.com>
2023-06-27 06:36:22 -0300

    filtros listos

--- src/main/resources/static/js/filtro.js
+++
@@ -1,6 +0,0 @@
-//Obtengo la tabla donde se muestran todos los dispositivos
-const tabla = document.getElementById('tablaDispositivos');
-
-//obtengo el filtro
-const filtro = document.getElementById('filtroDispositivo');
-console.log(filtro);
