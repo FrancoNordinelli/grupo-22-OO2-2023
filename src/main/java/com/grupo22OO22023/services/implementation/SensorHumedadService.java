a761aeb7020abe71a6d34753772ae5f84f862c49
FrancoNordinelli <imnordi@gmail.com>
2023-06-27 01:26:04 -0300

    arreglos de beans

--- src/main/java/com/grupo22OO22023/services/implementation/SensorHumedadService.java
+++ src/main/java/com/grupo22OO22023/services/implementation/SensorHumedadService.java
@@ -16,7 +16,7 @@ public class SensorHumedadService implements ISensorHumedadService {
 	private ModelMapper mp = new ModelMapper();
 	
 	@Autowired
-	@Qualifier("shRepository")
+	@Qualifier("sensorHumedadRepository")
 	private ISensorHumedadRepository shRepository;
 
 	@Override
