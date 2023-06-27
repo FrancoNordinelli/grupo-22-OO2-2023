cd9feb16ff11f0780c5e51a216f2811ac555a6c5
FrancoNordinelli <imnordi@gmail.com>
2023-06-27 01:15:13 -0300

    impl. sh service

--- src/main/java/com/grupo22OO22023/services/implementation/SHEventoService.java
+++ src/main/java/com/grupo22OO22023/services/implementation/SHEventoService.java
@@ -1,5 +1,51 @@
 package com.grupo22OO22023.services.implementation;
 
-public class SHEventoService {
+import org.modelmapper.ModelMapper;
+import org.modelmapper.internal.bytebuddy.asm.Advice.Return;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.grupo22OO22023.entities.SHEvento;
+import com.grupo22OO22023.entities.SPEvento;
+import com.grupo22OO22023.entities.SensorHumedad;
+import com.grupo22OO22023.models.SHEventoModel;
+import com.grupo22OO22023.repositories.ISHEventoRepository;
+import com.grupo22OO22023.services.ISHEventoService;
+
+@Service("SHEvento")
+public class SHEventoService implements ISHEventoService {
+	
+	private ModelMapper mp = new ModelMapper();
+	
+	@Autowired
+	@Qualifier("shEventoRepository")
+	private ISHEventoRepository shEventoRepository;
+
+	@Override
+	public SHEventoModel insertOrUpdate(SHEventoModel shEventoModel) {
+		SHEvento sh = shEventoRepository.save(mp.map(shEventoModel, SHEvento.class));
+		
+		return mp.map(sh, SHEventoModel.class);
+	}
+
+	@Override
+	public boolean remove(int id) {
+		try {
+			shEventoRepository.deleteById(id);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	@Override
+	public SHEventoModel findByidEvento(int id) {
+		return mp.map(shEventoRepository.findById(id), SHEventoModel.class);
+	}
+
+	
+
+	
 
 }
