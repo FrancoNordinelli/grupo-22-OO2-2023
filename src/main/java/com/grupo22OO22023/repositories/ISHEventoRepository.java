3d6142101bdb16ec11db3075159213efe809449b
FrancoNordinelli <imnordi@gmail.com>
2023-06-27 01:13:42 -0300

    interface SH

--- src/main/java/com/grupo22OO22023/repositories/ISHEventoRepository.java
+++ src/main/java/com/grupo22OO22023/repositories/ISHEventoRepository.java
@@ -3,10 +3,12 @@ package com.grupo22OO22023.repositories;
 import java.io.Serializable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.grupo22OO22023.entities.SHEvento;
 
 
+@Repository("shEventoRepository")
 public interface ISHEventoRepository extends JpaRepository<SHEvento, Serializable> {
 
 	public abstract SHEvento findByIdEvento(int idEvento);
