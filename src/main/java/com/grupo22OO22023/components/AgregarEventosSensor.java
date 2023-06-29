4c3104c22f5a1b549e7e563ed3f1960504073dd3
FrancoNordinelli <imnordi@gmail.com>
2023-06-29 16:09:02 -0300

    temporizador y dispositivo ramndom

--- src/main/java/com/grupo22OO22023/components/AgregarEventosSensor.java
+++ src/main/java/com/grupo22OO22023/components/AgregarEventosSensor.java
@@ -1,13 +1,14 @@
 package com.grupo22OO22023.components;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 import java.util.Scanner;
 
 import org.modelmapper.ModelMapper;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-
 
 import com.grupo22OO22023.entities.SensorHumedad;
 import com.grupo22OO22023.models.SHEventoModel;
@@ -16,19 +17,18 @@ import com.grupo22OO22023.models.SensorHumedadModel;
 import com.grupo22OO22023.services.ISHEventoService;
 import com.grupo22OO22023.services.ISensorHumedadService;
 
-
 public class AgregarEventosSensor implements Runnable {
-	
-	
-	
+
 	@Qualifier("SensorHumedad")
 	private ISensorHumedadService shService;
 	@Qualifier("SHEvento")
 	private ISHEventoService shI;
-	
+
 	private ModelMapper mp = new ModelMapper();
-	
+
 	private final Scanner sc;
+
+	private Random random = new Random();
 
 	public AgregarEventosSensor(ISensorHumedadService shService, ISHEventoService shI) {
 		this.shService = shService;
@@ -39,18 +39,47 @@ public class AgregarEventosSensor implements Runnable {
 	@Override
 	public void run() {
 
-		Optional<SensorHumedadModel> sensor = shService.findById(1);
-		
-		System.out.println("Nombre del evento: ");
-		String nombreEvento = sc.nextLine();
+		while (true) {
+			
+			int id;
+			try {
+				id = getRandom();
+				Optional<SensorHumedadModel> sensor = shService.findById(id);
+				System.out.println("Nombre del evento: ");
+				String nombreEvento = sc.nextLine();
+				System.out.println("Regar? (true/false) :");
+				boolean regar = sc.nextBoolean();
+				SHEventoModel ev = new SHEventoModel(nombreEvento, LocalDateTime.now(), mp.map(sensor, SensorHumedad.class),
+						regar);
+				shI.insertOrUpdate(ev);
+				System.out.println(sensor);
 
-		System.out.println("Regar? (true/false) :");
-		boolean regar = sc.nextBoolean();
-		
-		SHEventoModel ev = new SHEventoModel(nombreEvento, LocalDateTime.now(), mp.map(sensor, SensorHumedad.class), regar);
-		System.out.println(sensor);
-
-		shI.insertOrUpdate(ev);
-
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
+	
+	
+	
+
+	private int getRandom() throws Exception {
+
+		int id = 0;
+		List<SensorHumedadModel> sensores = shService.getAll();
+		if (!sensores.isEmpty()) {
+			int randomIndex = random.nextInt(sensores.size());
+			id = sensores.get(randomIndex).getId();
+		} else {
+			throw new Exception("No hay dispositivos");
+		}
+		return id;
+	}
+
 }
